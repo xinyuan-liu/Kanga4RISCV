8c55a076f9f722f9d75a1a95239affca019b99e8
Xinyuan Liu <lxy_pku@qq.com>
2016-12-04 21:47:29 +0800

    delete useless files

--- a.py
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/python
-import os
-rootdir="./kg"
-for parent,dirnames,filenames in os.walk(rootdir):
-    for filename in filenames:
-        print filename
-        os.system('java -cp bin/ kanga.kgi < ./kg/'+filename+' > res2')
-        os.system('java -jar kgi.jar < ./kg/'+filename+'> res1')
-        os.system('diff res1 res2')
